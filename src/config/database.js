21ab807a3356512ac99167cc005f2b83c721cfbf
Laxman Pawar <lxp4567@gmail.com>
2024-07-23 19:46:09 +0530

    change

--- src/config/database.js
+++ src/config/database.js
@@ -4,10 +4,10 @@ require('dotenv').config(); // Add this line to load environment variables
 
 const sequelize = new Sequelize({
   dialect: 'mariadb', // You can change this to 'postgres' or 'sqlite' based on your database choice
-  host: process.env.DB_HOST ,
-  username: process.env.DB_USER ,
-  password: process.env.DB_PASSWORD ,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || '129.154.34.108',
+  username: process.env.DB_USER || 'ecommerce',
+  password: process.env.DB_PASSWORD || 'ecommerce12345',
+  database: process.env.DB_NAME || 'ecommerce',
   dialectOptions: {
     // for writing to database india kolkata time
     timezone: '+05:30',
