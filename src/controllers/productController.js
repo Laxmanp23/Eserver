763816160b87eff684e2730424e3b78bbca4c077
Laxman Pawar <lxp4567@gmail.com>
2024-07-23 19:00:44 +0530

    update code

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -111,7 +111,7 @@ exports.deleteProduct = async (req, res) => {
       return res.status(404).json({ message: "Product not found" });
     }
     await product.destroy();
-    res.status(204).send();
+    res.status(204).json();
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,9 +1,19 @@
 const user = require("../models/user");
 const { Op } = require("sequelize");
 const TokenStore = require("../utils/TokenStore");
+
 const authMiddleware = async (req, res, next) => {
- 
-  const token = req.header('Authorization').replace('Bearer ', '');
+  const authHeader = req.header("Authorization");
+  if (!authHeader) {
+    return res
+    .status(401)
+    .json({ error: "Unauthorized - No Authorization header provided" });
+  }
+  
+  const token = authHeader.replace("Bearer ", "");
+  // console.log("Token:", token);
+  // console.log(authHeader)
+
   try {
     const tokenData = await TokenStore.findOne({
       where: {
@@ -13,24 +23,20 @@ const authMiddleware = async (req, res, next) => {
         },
       },
     });
-    if (tokenData) {
-      
-      if (tokenData) {
-        req.user = tokenData.userId;
-        req.token = token;
-        console.log(req.user)
 
-        next();
-      } else {
-        return res.status(401).json({ error: "Unauthorized - Invalid User" });
-      }
-    } else {
+    if (!tokenData) {
       return res.status(401).json({ error: "Unauthorized - Invalid Token" });
     }
+
+    req.user = tokenData.userId;
+    req.token = token;
+    next();
   } catch (error) {
     console.error("Error in authMiddleware:", error);
     res.status(401).json({
-      message: "Not authorized to access this resource",});
+      message: "Not authorized to access this resource",
+    });
   }
 };
+
 module.exports = authMiddleware;
