d67bb1efdc6a8aab1ced562662ff1f56dcf93784
Laxman Pawar <lxp4567@gmail.com>
2024-07-23 19:03:52 +0530

    change

--- src/config/database.js
+++ src/config/database.js
@@ -14,6 +14,6 @@ const sequelize = new Sequelize({
     connectTimeout: 20000,
 
   },
-  logging: true, // Enable debug logging
+  // logging: true, // Enable debug logging
 });
 module.exports = sequelize;
